3a1b841f38bf2b73d601803ed4e25d4bcbf7e4bb
davidaustinm <david.austin.m@gmail.com>
2026-07-14 15:21:52 -0400

    Rename labels 'a' and 'b' to 'c' and 'd'

--- jslibrary/linearcomb1.js
+++ jslibrary/linearcomb1.js
@@ -48,12 +48,12 @@ var tb = mkslider([dx, 2-dx], 0.5, topmatrix, topupdate);
 ta.init(1);
 tb.init(0);
 
-var lta = new Label("a", [0,1.5]);
+var lta = new Label("c", [0,1.5]);
 lta.offset = [-5,-3]
 lta.font = "italic 16px arial";
 topmatrix.addPlotable(lta);
 
-var ltb = new Label("b", [0,0.5]);
+var ltb = new Label("d", [0,0.5]);
 ltb.offset = [-5,-3]
 ltb.font = "italic 16px arial";
 topmatrix.addPlotable(ltb);
